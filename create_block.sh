9b3512c9887ab89c37d2a7bf15b743215d7d7bd0
Zak <zak@beattiecole.com>
2018-05-26 11:41:56 -0700

    script

---
+++ create_block.sh
@@ -0,0 +1,318 @@
+#!/bin/bash
+
+
+
+echo " __    __     _                            _        "
+echo "/ / /\ \ \___| | ___ ___  _ __ ___   ___  | |_ ___  "
+echo "\ \/  \/ / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \ "
+echo " \  /\  /  __/ | (_| (_) | | | | | |  __/ | || (_) |"
+echo "  \/  \/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/ "
+echo "                                                   " 
+echo " __    __ _     _ _       _     _            _    "
+echo "/ / /\ \ \ |__ (_) |_ ___| |__ | | ___   ___| | __"
+echo "\ \/  \/ / '_ \| | __/ _ \ '_ \| |/ _ \ / __| |/ /"
+echo " \  /\  /| | | | | ||  __/ |_) | | (_) | (__|   < "
+echo "  \/  \/ |_| |_|_|\__\___|_.__/|_|\___/ \___|_|\_\"
+clear
+
+echo " _    _ _   ____                  _ "
+echo "| |  | (_) |  _ \                | |"
+echo "| |__| |_  | |_) | ___  _   _ ___| |"
+echo "|  __  | | |  _ < / _ \| | | / __| |"
+echo "| |  | | | | |_) | (_) | |_| \__ \_|"
+echo "|_|  |_|_| |____/ \___/ \__, |___(_)"
+echo "                         __/ |      "
+echo "                        |___/       "
+echo "You're doing an awesome job! Keep up the good work."
+echo "I"
+echo "I L"
+echo "I LO"
+echo "I LOV"
+echo "I LOVE"
+echo "I LOVE YOU!"
+clear
+
+echo "
+echo "                                    .:AMMMMMMMMMMA:."
+echo "                                  .AMMMMMMMMMMMHMMMMA."
+echo "                                 AMMMMMMMMMMMMMHMMMMMA"
+echo "                                :MMMMMMMMMMMHMHHHHMMMMA."
+echo "                                AMMMMIIIIIIIIIIIIIIIIIMA."
+echo "                               :MMMMIIIIII:.... ..IIIIIMA"
+echo "                               AHMMIIIIII::....   .:)IIMM"
+echo "                               MMMMIIIIIII::....   /..:MM"
+echo "                               MMMVIIIIII:::...   /...:MM"
+echo "                               VMHIIIIIII:::.      ...:MM"
+echo "                               :HHII::mMMMMm:.....mmm.IMM"
+echo "                               IIHI::::YIMIbHI :dIMIIP:MI"
+echo "                               IIIII:::::::.II :.::::..:I"
+echo "                               `IIII::::::.:II :.   ..::V"
+echo "                                VIIII:::::::II :......:A'"
+echo "                                `III:I:::::IHIII:  ..::V"
+echo "                                 VI::::::::::i.  . .::A"
+echo "                                 `II:::::.md888bm.:.::V"
+echo "                                  :IH::::::Y888P:..::A'"
+echo "                                  AIH\:::::::.  ..:::V"
+echo "                                 .MIIIH::::::.....::V"
+echo "                               .A::IIIIIIIIIIIIIIV.V"
+echo "                             .:M:::I:IIIIIII:::::...M"
+echo "                          .:I:::..II::III:::::::....:A."
+echo "                      .:A::...    I::::::::::::.....:. A:."
+echo "                    AI:.....     .:::::::::::::.....::    A:."
+echo "                 .:V:.......    ....::..:....::.....:... ....A:."
+echo "               .AMII::::::::::.:::..:::... .....   .............A."
+echo "             .A:::::......  ..:.::.....::... ...:::::. ..:::::::::A."
+echo "            AII::.......  ..::.:.....  . ::......:      .... .......A:."
+echo "          .AIII:::...     ::.......  ....  :...  ..::    ....... ....::A."
+echo "         .MIIII:::..     ::............... .:... ....::..:......   ....::A"
+echo "         AIIIIII:::.....I:..................  :      ............      ::IA"
+echo "        :MIIIII::::::::I:::::..............    ...................   .:::IIA"
+echo "        AHIIIIIII:::::H:::::................. :......................::::IIM:"
+echo "        MHIIIIIII:::IHII:::::...   .......  . ....................:::::::IIM"
+echo "       :MMHIIIIIIIIIMHII::::::...       ..... ... ... .............::::IIIIV"
+echo "       AMMHHIIIIIHIMHHHIII::::::....      ... :..      .. .......::::IIIIIA:"
+echo "      AHHHHHHHHIIIIMHHHHIIIII:::::....     .. ....          ....::::HIIIIIM'"
+echo "     AHHHHHHIII::IIMHHHHHIIIIII:::::....  ..  ::..          .....:HHIIIIIIM"
+echo "    :MHHHHHHHI:::IMHHHHHHIMMIIIII:::::::....  :....        ....::HHIIIIIIIV:"
+echo "    AHHHHIIHHII::IMHHHHHIMMoMIIIIIH:::::    :    ........:::.::HHIHH:IIIIIIA"
+echo "   :MHHHIIIIIIIIIIM:IHHHIHMMMIHHIIHHA:::... ::::.   ::.::::HHoIHHHIIIIIIIIIV:"
+echo "   AHHIIIIIIIIIIIIAAIIHHHHHHHHHHIII::::::     .   ...::::IIIHHHHH:IIIIIIIIIIA"
+echo "  AHIHIIIIIIIIIIIV  VIIIHHHHHHIIIIH::::::  ..:::.    ::IIIIIIIAII:IIIIIIIIIIM"
+echo " A:HIIIIIIIIIIIMV   `AII:IHHHIHIIIHHAm:::::::::::::::IIIIIIIIIHA:::IIIII:IIIM"
+echo " I:IIIIIIIIIIMV'     V:::IHIIH::III::::    .::::: ::::::::::::M V::IIIIIIIIIM"
+echo " I:IIII:::HIM'        V::IHHIH::HII::::::..:.::.    ::::..::IIM  V::IIIIIIIIV."
+echo " V:III:...:IM         `A:IHHHHmIHIHHAm::.....::.. .:::.:::::::V   V:IIIIIHIIIA"
+echo " :M:II::....:A         V:IIHH:HIIIHHV::::::.:::.:::::.::I::::A'    AIIHHHIIIIM"
+echo "  V:IIII:....:A        `A:IHH:HIIIII:::     ::::::::::::I::::V     MHHIIIIIIIM"
+echo "  :MIIII:::...:A        V:IHHmIIIIIIA:::.. ...:    :::::I.IIA'     MHIIIIIIIIM"
+echo "   VIIIIII::...:A       `AIHII:IIIIHHAm:::..::..    .::I::::M      MIIIIIIIIIV"
+echo "    V:IIII:::...:A       MHHIHmHIIIIIV:::..HHH:::...:::I:..IV.    .VIIIIIIIIA'"
+echo "     VIIII:I:::..:A      MIIHH:HIIIII::::.IMCMI.....:::I:...:A    AIIIIIIIIIM"
+echo "      VIIII::::::::b    .VIH:::HIIIIA:::::.::::.....:..:I...:M    MIIIIIIIIIV"
+echo "       `VIII::I.::::b   AIIH::::HIIIIHA::::::::.....I..:I...:M    M:IIIIIIIA:"
+echo "         VIII:::::::M:. M\IH::\::HIIIIH:::::I::::...I:::I::::M    MIIIIIIIIM'"
+echo "          VIII:::::I:HI:HI:::::II:HIIHV::I:::::::::I::::::.::M    V:IIIIIIIM"
+echo "           `VII::I::IV::VIA::::.I:HIIH::::I:I::::::::::I...::M  .AIIIIIIIIIV"
+echo "             VII:::V::::::VA::...I::IHHA:I:::I::::::::::....:MMA..::.:IIIIV"
+echo "             `VI:I::::::....MA:...I::HV::I:::::::::::::I ...A.  . ..:.IIIV"
+echo "               MIII::..:::....A....I::::::::.:::::::::I. . A:M8M....::IAV"
+echo "               HIII:::../M8A:.M... .I:::::::::::::::::. .:AMMIV...V.::V'"
+echo "               MI:I.....MMM88MMA:.  :I:I::::::::::.::::AMM88MV..AV..:A'"
+echo "               V:II::::.M::MMM88MMMA:.IIIII::::::::AMMM888V'..:M...V:V"
+echo "               `MII:::::M:::.`VM8888MMMMMMMMMMMMMMM8VK88V'...AV...V.:'"
+echo "                VIMI:V:::A::...`VM888888888888888888968V . AV.A..V..V"
+echo "                `MM::A`V: A:.... `VM88888888888888888MV  WV  A. V:VA'"
+echo "                 VHM.:A:V: A....  .`VM88888888888888V'.     A:.V:V:M"
+echo "                 `MM\.:A:V::A.. .....VM888888888888V..     A::V:V. V"
+echo "                  VIM:.IA:`WW.........VM8888888888V....  . WW:.V :V"
+echo "                  `MHM.HIW:::..........VMM8888888M'........ WMVWWV"
+echo "                   `VIM.IW:::...........VMMM88888V:............IM"
+echo "                    `MIMII:::............VMMMM88V:::...........:M"
+echo "                     M:II:::::............VMMMMVI::::.........::M"
+echo "                     M::::::.... ....:.:::.MMMV:::::::.........:M"
+echo "                     M::I::::... .....:::::MMIII:::::::.......::M"
+echo "                     MIII:::....  ...::::::MIIII::::::::.:.:..::V"
+echo "                     MII:::::... ..::::::::MHIIII::::::::...:.:A:"
+echo "                     MIII:::.... ...:::::::MHII::I::..:::::::::M'"
+echo "                     MII::::::.  ..::::::::MIIIIIIII:..::::::::M"
+echo "                     MII::::::.  ..::::::::MIIIIIIII:.:::::::::M"
+echo "                     MIII:::::.  ...:::::::MIIIII:III::::::::::V"
+echo "                     MII::::::.  ...:::::::MIIIIIIIII:::::::::A:"
+echo "                     MII::::::.  .:.:::::::MHIIIIII:::::::::::M'"
+echo "                     MII:::::::.  :.:::::::MHHIIII:I::::::::::M"
+echo "                     MIII::::::.  :.::::I::MHHIIIII::::::::::A:"
+echo "                     VII::::::::  ..::::::MHHHHIIII::::::::I:M'"
+echo "                     :AII:::::::. .::::::V MHHHIIII:::::II::IV"
+echo "                    :MI::::::::. ..::::V  MHHHIII:::::III:IA:"
+echo "                     :MI::::::::: ..:::A'  MHHHIIII::IIIII:IM'"
+echo "                      VII::::::::  ::::M   MHHHIIIIIIIIIIIIIV"
+echo "                      :AI::::::::  ::::M  :VHHHIIIIIIIIIIIIA'"
+echo "                      :MI::::::::. ::::M  AHHHHIIIIIIIIIIIIV"
+echo "                       MI::::::::. ::::V  MHHHHIII::IIIIIHV"
+echo "                       MI:I::::::..:::A'  MHHHHIII:::::IIA'"
+echo "                       MIH::::..::::::V  :VHHHHIII:::::IIV"
+echo "                       MIHI:::..::::IA'  AHHHHHHII:::IIIA'"
+echo "                       MIHI:::. :::HHM   MIIHHHHII:::I:IM"
+echo "                      .VIIH::::::HHHIV   VIIIHHHIIH:::HIV"
+echo "                      AIIIIH::::HHH:A'   :AIIHHIIIIHHHIA'"
+echo "                     .VIIIIHHHIHHHIIV    `MIIIHIIIIIIIIV"
+echo "                     AHIIIIIHHHHHHIA'     MHHIIHHIIIIIA'"
+echo "                    .VHIIIIIIIHHH:IM      VHHHIIHHIIIIM"
+echo "                    AIHIIIIIIIIIHIIM      :AHHIIIHHIIIV."
+echo "                   AIIHIIIIIIIIIIHIM      `MHHIIIIIIIIIA"
+echo "                  .VIIHIIIIIIIIIHIIV:     .VHIIIIII:I:IM"
+echo "                  AIIIIIIIIIIIIHIIIIA     AHHIIII:::I::M"
+echo "                  MIIIIIIIIIIIHIIIIIM    .VHIHIII:I:I::M:"
+echo "                  VIIIIIIIIIIHIIIIIIM    AHIIIHIII:::::V:"
+echo "                  :AIIIIIIIIIIIIIIIIM   .VIIIIIHI::::::MA"
+echo "                  `MIIIIIIIIIIIIIIHA'   AIIIIIIHI:::::IMV"
+echo "                   VIIIIIIIIIIIIHHHV    MHIIII:II.:::IIA:"
+echo "                   `VIIIIIIIIIIHHHV     MHIIIIIII.:::IIM"
+echo "                    :AIIIIIIIIHHHM'     MHHIII:II::II:IM"
+echo "                     MIIIIIIHHHHHV      MHHIII:II::IIIIV"
+echo "                     MIIIIIIHHHHV       VAHIII:I:::IIIA:"
+echo "                     VHIIIIIIHHA'       :MHII::I:::IIIM"
+echo "                     :AIIIIIIHHM         MHI::II:IIIIIM"
+echo "                     `MHIIIIIIIM         MHII:I::IIIIIV"
+echo "                      MIIIIIIIIM         MHII:I:::IIIA'"
+echo "                      VHHIIIIIIM         MHII:I:II:IIV"
+echo "                      :AIIIIIIIM         MHII.I::IIIV"
+echo "                      `MIIIIIIIM         MHII:I:IIIA"
+echo "                       MHIIIIIIM         MHHII::IIIV"
+echo "                       MHIIIIIIV.        MHH:I::IIV"
+echo "                       MHHII:IIIA        MHI:I:IIA'"
+echo "                       MHHIIIIIIM        MHI:::IIV"
+echo "                       MHHIIIIIIM       :VI::::IA'"
+echo "                       MHHIIIIIIM       AMM::::IV"
+echo "                      .VII:::::IM       MHII:::IM"
+echo "                      AI:I::::IIM      .VIII:::IV"
+echo "                     AIIII::::IIM      AHIII:::::A"
+echo "                     MIIII::::::V     .VHIII:::::V."
+echo "                     MII:::::::A'     AHHIII::::::A."
+echo "                    .V:I:::::IIM      VHHII:I:::::::A."
+echo "                    AI:I:II:::IM       VAHIIII:::.:::A."
+echo "                   .M:::::::::IM        `"VAHIII:.:::::A."
+echo "                   M.....::I:::M            `"VAI:::.:::VMA."
+echo "                   VA.......:::V:              `VMI::..:::VA)"
+echo "                   `M.Q.Q.Q./..:D               `VM:::::D:::D)"
+echo "                     V(E(E(EIIMD                   `VMIID)MD)"
+clear 
+
+function usage {
+        cat <<EOM
+     Usage: create_block -c <chainId> -n <networkid>
+     Example: create_block -c 17835211 -n 178354
+EOM
+    exit 1
+}
+
+while getopts ":c:n:h" optKey; do
+    case $optKey in
+        c)
+            c=$OPTARG
+            ;;
+	n)
+            n=$OPTARG
+            ;;
+        h|*)
+            usage
+            ;;
+    esac
+done
+
+c=696969
+n=17835
+
+if [ -f "/tmp/CustomGenesis.json" ]; then
+	echo "Genesis file already exists! Do you want to remove it?"
+        select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) rm /tmp/CustomGenesis.json
+            No ) exit;;
+
+else
+cat <<EOF > /tmp/CustomGenesis.json
+{
+    "config": {
+        "chainId": $c,
+        "homesteadBlock": 0,
+        "eip155Block": 0,
+        "eip158Block": 0
+    },
+    "difficulty": "0x4000",
+    "gasLimit": "0x2100000",
+    "alloc": {}
+}
+EOF
+
+fi
+
+
+
+for i in {1..3}; do
+
+count=$i
+# set the ip address for the enode
+nodeIP=10.$i.100.100
+
+# Create the node directory
+mkdir /home/appo/node$i
+
+# Load the CustomeGenesis file
+gubiq --datadir /home/appo/node$i init /tmp/CustomGenesis.json
+
+# Create a new account/wallet
+gubiq --password passwd.file account new >> /home/appo/node$i/wallet
+
+# Get the enode from console and drop out of console
+gubiq --rpc --datadir /home/appo/node$i/ --networkid $n console >& /tmp/node$i.output
+
+# Inject the IP Adress into the enode string
+enode=`cat /tmp/node$i.output | grep enode | awk '{print $5}'| sed "s/\[::\]/$nodeIP/g"`
+
+# Write the enode info to the node directory
+echo $enode >> /home/appo/node$i/enode
+
+# Create the static nodes file
+rm /tmp/static-nodes.json
+
+if [ -f "/tmp/static-nodes.json" ]; then
+## ------------------------------------#
+cat >> /tmp/static-nodes.json <<EOL
+"$enode",
+EOL
+## ------------------------------------#
+else
+## ------------------------------------#
+echo "[" > /tmp/static-nodes.json
+cat >> /tmp/static-nodes.json <<EOL
+"$enode",
+EOL
+fi
+
+done
+
+# Close static nodes file
+sed -i '$s/\",/\"/g' /tmp/static-nodes.json
+echo "]" >> /tmp/static-nodes.json
+
+# Copy static nodes to each node directory
+one=1
+
+while [ "$one" -le "$count" ]; do
+cp /tmp/static-nodes.json /home/appo/node$one
+(( one++ ))
+done
+
+# allow ssh 
+for i in {1..3}; do
+count=$i
+ssh-copy-id node$i 
+done
+
+# Copy datadir to each peer node
+for i in {1..3}; do
+count=$i
+scp -r /home/appo/node$i node$i:/home/appo
+done
+
+expect "appo@node1's password:"
+send "w@ntest"
+expect "appo@node2's password:"
+send "w@ntest"
+expect "appo@node3's password:"
+send "w@ntest"
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
