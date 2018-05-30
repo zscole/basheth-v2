248beff75e2ad48a745af53209ceef8462ce92f5
Zak <zak@beattiecole.com>
2018-05-29 17:49:09 -0700

    reset script

---
+++ reset.sh
@@ -0,0 +1,15 @@
+while [ $e -le $N ]
+do
+        function expect_password {
+        expect -c "\
+         set timeout 90
+         set env(TERM)
+         spawn $1
+         expect \"*password:\"
+         send \"w@ntest\r\"
+        expect eof
+        "
+        }
+    expect_password "ssh -t -o StrictHostKeyChecking=no node$e rm -R /home/appo/node$e"
+(( e++ ))
+done
