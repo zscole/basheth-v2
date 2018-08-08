be63f9619759969a68cab5c7562c494b73fcd585
Zak <zak@beattiecole.com>
2018-08-08 09:08:27 -0700

    added templates and bootnode

---
+++ bootnode.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+docker stop ethereum-bootnode
+docker rm ethereum-bootnode
+IMGNAME="ethereum/client-go:alltools-v1.7.3"
+DATA_ROOT=${DATA_ROOT:-$(pwd)}
+
+mkdir -p $DATA_ROOT/.bootnode
+if [ ! -f $DATA_ROOT/.bootnode/boot.key ]; then
+    echo "$DATA_ROOT/.bootnode/boot.key not found, generating..."
+    docker run --rm \
+        -v $DATA_ROOT/.bootnode:/opt/bootnode \
+        $IMGNAME bootnode --genkey /opt/bootnode/boot.key
+    echo "...done!"
+fi
+# creates ethereum network
+[ ! "$(docker network ls | grep ethereum)" ] && docker network create ethereum
+[[ -z $BOOTNODE_SERVICE ]] && BOOTNODE_SERVICE="127.0.0.1"
+docker run -d --name ethereum-bootnode \
+    -v $DATA_ROOT/.bootnode:/opt/bootnode \
+    --network ethereum \
+    $IMGNAME bootnode --nodekey /opt/bootnode/boot.key --verbosity=3 "$@"
+# --addr "$BOOTNODE_SERVICE:30301" "$@"
+
+
---
+++ getbootnodeurl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ENODE_LINE=$(docker logs ethereum-bootnode 2>&1 | grep enode | head -n 1)
+MYIP=$(docker exec ethereum-bootnode ifconfig eth0 | awk '/inet addr/{print substr($2,6)}')
+ENODE_LINE=$(echo $ENODE_LINE | sed "s/127\.0\.0\.1/$MYIP/g" | sed "s/\[\:\:\]/$MYIP/g")
+echo "enode:${ENODE_LINE#*enode:}"
